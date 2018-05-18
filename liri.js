64e839ee59dfb8497de1c9897f5df784397c2731
jealob <jealob@gmail.com>
2018-05-18 18:38:06 -0500

    Adds Twitter, Spotify, and OMDB API calls. NPM returns response

--- liri.js
+++ liri.js
@@ -1,17 +1,131 @@
 require("dotenv").config();
 let keys = require('./keys.js');
+let fs = require("fs");
 let Spotify = require('node-spotify-api');
-let spotify = new Spotify({
-    id: <your spotify client id>,
-    secret: <your spotify client secret>
-  });
-   
-  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+let Twitter = require('twitter');
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+let request = require("request");
+
+// Grab User's Input
+let userInput = (process.argv).slice(2);
+let action = userInput[0];
+let mediaName = "";
+for (let i = 1; i < userInput.length; i++) {
+  mediaName += " " + userInput[i];
+}
+
+//Check if there is an input argument  
+debugger;
+if (!action) {
+  // No Input action, get action and media from random.txt file
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    // Error Handler
+    if (error) {
+      return console.log(error);
     }
-   
-  console.log(data); 
+    myData = data.split(",");
+  // Pass input action and mediaName
+    findAPI(myData[0], myData[1]);
   });
-let spotify = new spotify(keys.spotify);
-let client = new Twitter (keys.twitter);
+}
+  // Pass input action and mediaName
+else {
+  findAPI(action, mediaName);
+};
+
+function findAPI(act, media) {
+  // Determine the API to call...
+  console.log(`Requesting ${act}...`);
+  switch (act) {
+    case "my-tweets":
+      twitterFunc(act);
+      break;
+    case "spotify-this-song":
+      (media) ? spotifyFunc(act, media) : spotifyFunc(act, 'Changes.');
+      break;
+    case "movie-this":
+      (media) ? OMDBFunc(act, media) : OMDBFunc(act, 'Mr. Nobody.');
+      break;
+    default:
+      console.log("This shouldn't be happening");
+  }
+}
+
+function twitterFunc(act) {
+  console.log("Searching Twitter...");
+  console.log("Getting Response...");
+  client.get('statuses/user_timeline', function (error, tweets, response) {
+    if (error) {
+      console.log(error)
+    }
+    else {
+      let userTweets = tweets;
+      console.log(userTweets[0]);
+      console.log(
+        `\nTwitter Handler: @ ${userTweets[0].user.screen_name}
+        \nUser Name: @ ${userTweets[0].user.name}
+        \n\n-----Tweets-----
+        `);
+        for (let i in  userTweets){
+          console.log(`
+        \nTweet ${(parseInt(i))+1}: ${userTweets[i].text}
+        \ntweet Time : ${userTweets[i].created_at}\n
+    `);
+        }
+        // The favorites.
+      // console.log(JSON.parse(response));  // Raw response object.
+    };
+  });
+}
+
+function spotifyFunc(act, media) {
+  console.log("Searching Spotify...");
+  console.log("Getting Response...");
+  spotify.search({ type: 'track', query: media })
+    .then(function (response) {
+      let musicInfo = response.tracks.items[0];
+      console.log(
+        `\nArtist(s): ${musicInfo.artists[0].name}
+        \nThe Song's Name: ${musicInfo.name}
+        \nAlbum : ${musicInfo.album.name}
+        \nDuration (Minutes): ${parseFloat((musicInfo.duration_ms / 60000).toFixed(2))} 
+        \nPreview Link from Spotify: ${musicInfo.external_urls.spotify}
+        \nPopularity : ${musicInfo.popularity}
+    `);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function OMDBFunc(act, media) {
+  console.log("Searching OMDB...");
+  let queryUrl = "http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=trilogy";
+  request(queryUrl, function (error, response, body) {
+    console.log("Getting Response...");
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+      let movieInfo = JSON.parse(body);
+      // Check whether or not the movie exist
+      if (!movieInfo.Error) {
+        // If movie exist print movie information
+        console.log(
+          `\nTitle of the movie: ${movieInfo.Title}
+         \nDate the movie was Released: ${movieInfo.Released}
+         \nIMDB Rating of the movie.: ${(movieInfo.Ratings[0]) ? movieInfo.Ratings[0].Value : "N/A"}
+         \nRotten Tomatoes Rating of the movie.: ${(movieInfo.Ratings[1]) ? movieInfo.Ratings[1].Value : "N/A"}
+         \nCountry where the movie was produced.: ${movieInfo.Country}
+         \nLanguage of the movie.: ${movieInfo.Language}
+         \nPlot of the movie: ${movieInfo.Plot}
+         \nActors in the movie.: ${movieInfo.Actors}
+      `);
+      }
+      else {
+        // Else print value of Error"Movie not found!"
+        console.log(movieInfo.Error);
+      }
+    }
+  });
+}
+
